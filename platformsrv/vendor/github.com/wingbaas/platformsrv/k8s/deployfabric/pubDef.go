4b28e5d75bc1fb71da81718a396f2c9a7caef44c
石 显锋 <shixianfeng@chainnova.com>
2020-04-10 18:58:07 +0800

    new file init

---
+++ platformsrv/vendor/github.com/wingbaas/platformsrv/k8s/deployfabric/pubDef.go
@@ -0,0 +1,377 @@
+
+package deployfabric
+
+import (
+	"fmt"
+	"encoding/json"
+	"github.com/wingbaas/platformsrv/logger"
+	"github.com/wingbaas/platformsrv/k8s"
+	"github.com/wingbaas/platformsrv/utils"
+	"github.com/wingbaas/platformsrv/certgenerate/fabric"
+)
+
+const (
+	KUBERNETES_DEPLOY_FAILED	string	=	"Failure"
+)
+
+//service struct define
+
+type MetadataService struct {
+	Name string `json:"name"`
+}
+
+type PortSpecService struct {
+	Name string `json:"name"`
+	Port int `json:"port"`
+	TargetPort int `json:"targetPort"`
+}
+
+type SelectorSpecService struct {
+	App string `json:"app"`
+}
+
+type SpecService struct {
+	Type string `json:"type"`
+	Ports []PortSpecService `json:"ports"`
+	Selector SelectorSpecService `json:"selector"`
+}
+
+//deployment struct define 
+
+// type Deployment struct {
+// 	APIVersion string `json:"apiVersion"`
+// 	Kind string `json:"kind"`
+// 	Metadata struct {
+// 		Name string `json:"name"`
+// 	} `json:"metadata"`
+// 	Spec struct {
+// 		Replicas int `json:"replicas"`
+// 		Selector struct {
+// 			MatchLabels struct {
+// 				App string `json:"app"`
+// 			} `json:"matchLabels"`
+// 		} `json:"selector"`
+// 		Strategy struct {
+// 			Type string `json:"type"`
+// 		} `json:"strategy"`
+// 		Template struct {
+// 			Metadata struct {
+// 				Labels struct {
+// 					App string `json:"app"`
+// 				} `json:"labels"`
+// 			} `json:"metadata"`
+// 			Spec struct {
+// 				Affinity struct {
+// 					NodeAffinity struct {
+// 						PreferredDuringSchedulingIgnoredDuringExecution []struct {
+// 							Preference struct {
+// 								MatchExpressions []struct {
+// 									Key string `json:"key"`
+// 									Operator string `json:"operator"`
+// 									Values []string `json:"values"`
+// 								} `json:"matchExpressions"`
+// 							} `json:"preference"`
+// 							Weight int `json:"weight"`
+// 						} `json:"preferredDuringSchedulingIgnoredDuringExecution"`
+// 					} `json:"nodeAffinity"`
+// 				} `json:"affinity"`
+// 				Containers []struct {
+// 					Args []string `json:"args"`
+// 					Env []struct {
+// 						Name string `json:"name"`
+// 						Value string `json:"value"`
+// 					} `json:"env"`
+// 					Image string `json:"image"`
+// 					ImagePullPolicy string `json:"imagePullPolicy"`
+// 					Name string `json:"name"`
+// 					Ports []struct {
+// 						ContainerPort int `json:"containerPort"`
+// 					} `json:"ports"`
+// 					Resources struct {
+// 						Requests struct {
+// 							CPU string `json:"cpu"`
+// 							Memory string `json:"memory"`
+// 						} `json:"requests"`
+// 					} `json:"resources"`
+// 					VolumeMounts []struct {
+// 						MountPath string `json:"mountPath"`
+// 						Name string `json:"name"`
+// 					} `json:"volumeMounts"`
+// 				} `json:"containers"`
+// 				ImagePullSecrets interface{} `json:"imagePullSecrets"`
+// 				RestartPolicy string `json:"restartPolicy"`
+// 				Volumes []struct {
+// 					Name string `json:"name"`
+// 					Nfs struct {
+// 						Path string `json:"path"`
+// 						Server string `json:"server"`
+// 					} `json:"nfs"`
+// 				} `json:"volumes"`
+// 			} `json:"spec"`
+// 		} `json:"template"`
+// 	} `json:"spec"`
+// }
+
+
+type LabelsSt struct {
+	App string `json:"app"`
+}
+
+type StrategySt struct {
+	Type string `json:"type"`
+}
+
+type MetadataTemplateSt struct {
+	Labels LabelsSt `json:"labels"`
+}
+
+type MatchPreferenceExeAffinitySpecTemplateSt struct {
+	Key string `json:"key"`
+	Operator string `json:"operator"`
+	Values []string `json:"values"`
+}
+
+type PreferenceExeAffinitySpecTemplateSt struct {
+	MatchExpressions []MatchPreferenceExeAffinitySpecTemplateSt `json:"matchExpressions"`
+} 
+
+type ExeAffinitySpecTemplateSt struct {
+	Weight int `json:"weight"`
+	Preference PreferenceExeAffinitySpecTemplateSt `json:"preference"`
+}
+
+type NodeAffinityAffinitySpecTemplateSt struct {
+	PreferredDuringSchedulingIgnoredDuringExecution []ExeAffinitySpecTemplateSt `json:"preferredDuringSchedulingIgnoredDuringExecution"`
+}
+
+type AffinitySpecTemplateSt struct {
+	NodeAffinity NodeAffinityAffinitySpecTemplateSt `json:"nodeAffinity"`
+}
+
+type RequestsResourceContainerSpecTemplateSt struct {
+	Memory string `json:"memory"`
+	CPU string `json:"cpu"`
+}
+
+type ResourceContainerSpecTemplateSt struct {
+	Requests RequestsResourceContainerSpecTemplateSt `json:"requests"`
+}
+
+type EnvContainerSpecTemplateSt struct {
+	Name string `json:"name"`
+	Value string `json:"value"`
+}
+
+type PortContainerSpecTemplateSt struct {
+	ContainerPort int `json:"containerPort"`
+}
+
+type VolumeContainerSpecTemplateSt struct {
+	MountPath string `json:"mountPath"`
+	Name string `json:"name"`
+	SubPath string `json:"subPath,omitempty"`
+}
+
+type ContainerSpecTemplateSt struct {
+	Name string `json:"name"`
+	Image string `json:"image"`
+	ImagePullPolicy string `json:"imagePullPolicy"`
+	Resources ResourceContainerSpecTemplateSt `json:"resources"`
+	Args []string `json:"args"`
+	Env []EnvContainerSpecTemplateSt `json:"env"`
+	Ports []PortContainerSpecTemplateSt `json:"ports"`
+	VolumeMounts []VolumeContainerSpecTemplateSt `json:"volumeMounts"`
+} 
+
+type ImagePullSecretSpecTemplateSt struct {
+	Name string `json:"name"`
+}
+
+type NfsVolumeSpecTemplateSt struct {
+	Server string `json:"server"`
+	Path string `json:"path"`
+}
+
+type EmptyDirVolumeSpecTemplateSt struct {
+}
+
+type VolumeSpecTemplateSt struct {
+	Name string `json:"name"`
+	//EmptyDir EmptyDirVolumeSpecTemplateSt `json:"emptyDir,omitempty"`
+	Nfs NfsVolumeSpecTemplateSt `json:"nfs,omitempty"`
+}
+
+type SpecTemplateSt struct {
+	Affinity AffinitySpecTemplateSt `json:"affinity"`
+	Containers []ContainerSpecTemplateSt `json:"containers"`
+	RestartPolicy string `json:"restartPolicy"`
+	ImagePullSecrets []ImagePullSecretSpecTemplateSt `json:"imagePullSecrets"`
+	Hostname string `json:"hostname"`
+	Volumes []VolumeSpecTemplateSt `json:"volumes"`
+}
+
+type TemplateSt struct {
+	Metadata MetadataTemplateSt `json:"metadata"`
+	Spec SpecTemplateSt `json:"spec"`
+} 
+type MatchLabelSt struct {
+	App	string `json:"app"`
+}
+
+type SelectorSt struct {
+	MatchLabels MatchLabelSt `json:"matchLabels"`
+}
+
+type SpecSt struct {
+	Selector SelectorSt `json:"selector"`
+	Replicas int `json:"replicas"`
+	Strategy StrategySt `json:"strategy"`
+	Template TemplateSt `json:"template"`
+}
+
+type MetadataDeployMent struct {
+	Name string `json:"name"` 
+}
+
+type DeployResult struct {
+	Kind string `json:"kind"`
+	APIVersion string `json:"apiVersion"`
+	Metadata interface{} `json:"metadata"`
+	Status interface{} `json:"status"`
+	Message string `json:"message"`
+	Reason string `json:"reason"`
+	Details struct {
+		Name string `json:"name"`
+		Group string `json:"group"`
+		Kind string `json:"kind"`
+	} `json:"details"`
+	Code int `json:"code"` 
+}
+
+func CreateDeployment(clusterId string,namespaceId string,deployObj interface{})([]byte,error) { 
+	bytes, err := json.Marshal(deployObj) 
+	if err != nil {
+		logger.Errorf("CreateDeployment: Marshal deployment error,%v",err) 
+		return nil,fmt.Errorf("CreateDeployment: Marshal deployment error,%v",err)
+	}
+	bl,yamlStr := fabric.JsonToYaml(string(bytes))
+	if !bl {
+		logger.Errorf("CreateDeployment: json2yaml str error") 
+		return nil,fmt.Errorf("CreateDeployment: json2yaml str error")
+	}
+	cluster,_ := k8s.GetCluster(clusterId)
+	if cluster == nil {
+		logger.Errorf("CreateDeployment: cluster nil,cluster id =%s",clusterId)
+		return nil,fmt.Errorf("CreateDeployment: cluster nil,cluster id =%s",clusterId)
+	}
+	reqUrl := cluster.Addr + k8s.API_APP + k8s.NAMESPACES + "/" + namespaceId + "/" + k8s.DEPLOYMENTS
+	bytes,err = utils.RequestWithCertAndBody(reqUrl,utils.REQ_POST,cluster.Cert,cluster.Key,yamlStr)
+	if err != nil { 
+		logger.Errorf("CreateDeployment: RequestWithCertAndBody err,%v", err)
+		return nil,fmt.Errorf("CreateDeployment: RequestWithCertAndBody err,%v", err)
+	}
+	var result DeployResult 
+	err = json.Unmarshal(bytes, &result)
+	if err != nil { 
+		logger.Errorf("CreateDeployment: create result err,%v", err)
+		return nil,fmt.Errorf("CreateDeployment: create result err,%v", err)
+	}
+	status,ok := (result.Status).(string)
+	if ok {
+		if status == KUBERNETES_DEPLOY_FAILED {
+			logger.Errorf("CreateDeployment: create failed,result=%s",string(bytes))
+			logger.Errorf("CreateDeployment: create failed,yamlstr=%s",yamlStr)
+			return nil,fmt.Errorf("CreateDeployment: create failed,result=%s",string(bytes))
+		}
+	}
+	logger.Debug("CreateDeployment: create success,result str=")
+	logger.Debug(string(bytes))
+	return nil,nil
+}
+
+func DeleteDeployment(clusterId string,namespaceId string,deployName string)([]byte,error) {
+	cluster,_ := k8s.GetCluster(clusterId)
+	if cluster == nil {
+		logger.Errorf("DeleteDeployment: cluser nil,cluser id =%s",clusterId)
+		return nil,fmt.Errorf("DeleteDeployment: cluster nil,cluster id =%s",clusterId)
+	}
+	reqUrl := cluster.Addr + k8s.API_APP + k8s.NAMESPACES + "/" + namespaceId + "/" + k8s.DEPLOYMENTS + "/" + deployName
+	bytes,err := utils.RequestWithCert(reqUrl,utils.REQ_DELETE,cluster.Cert,cluster.Key)
+	if err != nil { 
+		logger.Errorf("DeleteDeployment: RequestWithCertAndBody err,%v", err)
+		return nil,fmt.Errorf("DeleteDeployment: RequestWithCertAndBody err,%v", err)
+	}
+	var result interface{} 
+	err = json.Unmarshal(bytes, &result)
+	if err != nil { 
+		logger.Errorf("DeleteDeployment: delete result err,%v", err)
+		return nil,fmt.Errorf("DeleteDeployment: delete result err,%v", err)
+	}
+	logger.Debug("DeleteDeployment: delete success,result=,%v", result)
+	return nil,nil 
+}
+
+func CreateService(clusterId string,namespaceId string,serviceObj interface{})([]byte,error) {
+	bytes, err := json.Marshal(serviceObj)
+	if err != nil {
+		logger.Errorf("CreateService: Marshal service error,%v",err) 
+		return nil,fmt.Errorf("CreateService: Marshal service error,%v",err)
+	}
+	bl,yamlStr := fabric.JsonToYaml(string(bytes))
+	if !bl {
+		logger.Errorf("CreateService: json2yaml str error") 
+		return nil,fmt.Errorf("CreateService: json2yaml str error")
+	}
+	cluster,_ := k8s.GetCluster(clusterId)
+	if cluster == nil {
+		logger.Errorf("CreateService: cluster nil,cluster id =%s",clusterId)
+		return nil,fmt.Errorf("CreateService: cluster nil,cluster id =%s",clusterId)
+	}
+	reqUrl := cluster.Addr + k8s.API_V1 + k8s.NAMESPACES + "/" + namespaceId + "/" + k8s.SERVICES
+	bytes,err = utils.RequestWithCertAndBody(reqUrl,utils.REQ_POST,cluster.Cert,cluster.Key,yamlStr)
+	if err != nil { 
+		logger.Errorf("CreateService: RequestWithCertAndBody err,%v", err)
+		return nil,fmt.Errorf("CreateService: RequestWithCertAndBody err,%v", err)
+	}
+	var result DeployResult 
+	err = json.Unmarshal(bytes, &result)
+	if err != nil { 
+		logger.Errorf("CreateService: create result err,%v", err)
+		return nil,fmt.Errorf("CreateService: create result err,%v", err)
+	}
+	status,ok := (result.Status).(string)
+	if ok {
+		if status == KUBERNETES_DEPLOY_FAILED { 
+			logger.Errorf("CreateService: create failed,result=%s",string(bytes))
+			logger.Errorf("CreateService: create failed,yamlstr=%s",yamlStr)
+			return nil,fmt.Errorf("CreateService: create failed,result=%s",string(bytes))
+		}
+	}
+	logger.Debug("CreateService: create success,result str=")
+	logger.Debug(string(bytes))
+	return nil,nil 
+}
+
+func DeleteService(clusterId string,namespaceId string,serviceName string)([]byte,error) { 
+	cluster,_ := k8s.GetCluster(clusterId)
+	if cluster == nil {
+		logger.Errorf("DeleteService: cluser nil,cluser id =%s",clusterId)
+		return nil,fmt.Errorf("DeleteService: cluster nil,cluster id =%s",clusterId)
+	}
+	reqUrl := cluster.Addr + k8s.API_V1 + k8s.NAMESPACES + "/" + namespaceId + "/" + k8s.SERVICES + "/" + serviceName
+	bytes,err := utils.RequestWithCert(reqUrl,utils.REQ_DELETE,cluster.Cert,cluster.Key)
+	if err != nil { 
+		logger.Errorf("DeleteService: RequestWithCertAndBody err,%v", err)
+		return nil,fmt.Errorf("DeleteService: RequestWithCertAndBody err,%v", err)
+	}
+	var result interface{} 
+	err = json.Unmarshal(bytes, &result)
+	if err != nil { 
+		logger.Errorf("DeleteService: delete result err,%v", err)
+		return nil,fmt.Errorf("DeleteService: delete result err,%v", err)
+	}
+	logger.Debug("DeleteService: delete success,result=,%v", result)
+	return nil,nil
+}
+
+
---
+++ platformsrv/vendor/github.com/wingbaas/platformsrv/k8s/deployfabric/zookeeperDeployment.go
@@ -0,0 +1,124 @@
+
+package deployfabric
+import (
+	"os"
+	"github.com/wingbaas/platformsrv/utils"
+)
+
+type ZookeeperDeployMent struct {
+	APIVersion string `json:"apiVersion"`
+	Kind string `json:"kind"`
+	Metadata MetadataDeployMent `json:"metadata"`
+	Spec SpecSt `json:"spec"`  
+} 
+
+func CreateZookeeperDeployment(clusterId string,namespaceId string,chainId string,zkId string,image string,zookeeperName string)([]byte,error) {
+	zkDeployMent :=  ZookeeperDeployMent {
+		APIVersion: "apps/v1",
+		Kind: "Deployment",
+		Metadata: MetadataDeployMent{
+			Name: zookeeperName,
+		},
+		Spec: SpecSt{
+			Selector: SelectorSt{
+				MatchLabels: MatchLabelSt{
+					App: zookeeperName,
+				},
+			},
+			Replicas: 1,
+			Strategy: StrategySt{
+				Type: "Recreate",
+			},
+			Template: TemplateSt{
+				Metadata: MetadataTemplateSt{
+					Labels: LabelsSt{
+						App: zookeeperName,
+					},
+				},
+				Spec: SpecTemplateSt{
+					Affinity: AffinitySpecTemplateSt{
+						NodeAffinity: NodeAffinityAffinitySpecTemplateSt{
+							PreferredDuringSchedulingIgnoredDuringExecution: []ExeAffinitySpecTemplateSt{
+								{
+									Weight: 10,
+									Preference: PreferenceExeAffinitySpecTemplateSt{
+										MatchExpressions: []MatchPreferenceExeAffinitySpecTemplateSt{
+											{
+												Key: "team",
+												Operator: "In",
+												Values: []string{"baas"},
+											},
+										},		
+									},
+								},	
+							},
+						},
+					},
+					Containers: []ContainerSpecTemplateSt{
+						{
+							Name: zookeeperName,
+							Image: image,
+							ImagePullPolicy: "IfNotPresent",
+							Resources: ResourceContainerSpecTemplateSt{
+								Requests: RequestsResourceContainerSpecTemplateSt{
+									Memory: "256Mi",
+									CPU: "128m",
+								},
+							},
+							Env: []EnvContainerSpecTemplateSt{
+								{
+									Name: "ZOO_MY_ID",
+									Value: zkId,
+								},
+								{
+									Name: "ZOO_SERVERS",
+									Value: "server.1=zookeeper1:2888:3888 server.2=zookeeper2:2888:3888 server.3=zookeeper3:2888:3888",
+								},
+							},
+							Ports: []PortContainerSpecTemplateSt{
+								{
+									ContainerPort: 2181,
+								},
+								{
+									ContainerPort: 2888,
+								},
+								{
+									ContainerPort: 3888,
+								},	
+							},
+							VolumeMounts: []VolumeContainerSpecTemplateSt{
+								{
+									MountPath: "/data",
+									Name: "zk-data-store",
+									SubPath: clusterId + "/" + chainId + "/DataStore/" + zookeeperName + "/data",
+								}, 
+							},
+						},
+					},
+					Hostname: zookeeperName,
+					RestartPolicy: "Always",
+					Volumes: []VolumeSpecTemplateSt{
+						{
+							Name: "zk-data-store",
+							Nfs: NfsVolumeSpecTemplateSt{
+								Server: utils.BAAS_CFG.NfsInternalAddr,
+								Path: utils.BAAS_CFG.NfsBasePath,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	bytes, err := CreateDeployment(clusterId,namespaceId,zkDeployMent)
+	if err != nil {
+		certPath := utils.BAAS_CFG.BlockNetCfgBasePath + chainId
+		nfsPath :=  utils.BAAS_CFG.NfsLocalRootDir + chainId
+		os.RemoveAll(certPath)
+		os.RemoveAll(nfsPath)
+		DeleteNamespace(clusterId,namespaceId)
+	}
+	return bytes,err
+}
+
+
